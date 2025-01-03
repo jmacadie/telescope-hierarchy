0b549dcb3ac8faefd607f668d14d817c209ea124
James MacAdie <j.macadie@ttg.co.uk>
2025-01-03 10:43:51 +0000

    Move Telescope bits out into own module

--- lua/telescope-hierarchy/init.lua
+++ lua/telescope-hierarchy/init.lua
@@ -1,59 +1,11 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local themes = require("telescope.themes")
 local tree = require("telescope-hierarchy.tree")
-
----Convert the list of Nodes into a format Telescope can consume
----@param entry NodeLevel
----@return {value: Node, display: string, ordinal: string}
-local entry_maker = function(entry)
-  local prefix = ""
-  if entry.level > 1 then
-    prefix = string.rep("┆ ", entry.level - 2)
-    if entry.last then
-      prefix = prefix .. "└╴"
-    else
-      prefix = prefix .. "├╴"
-    end
-  end
-  local display = prefix .. entry.node.text
-  return {
-    value = entry,
-    display = display,
-    ordinal = display,
-  }
-end
-
--- our picker function: colors
-local show_hierarchy = function(results, opts)
-  opts = opts or {}
-  pickers
-    .new(opts, {
-      prompt_title = "colors",
-      finder = finders.new_table({
-        results = results,
-        entry_maker = entry_maker,
-      }),
-      -- sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr, map)
-        for _, mode in pairs({ "i", "n" }) do
-          for key, get_action in pairs(opts.mappings[mode] or {}) do
-            map(mode, key, get_action(prompt_bufnr))
-          end
-        end
-        return true -- include defaults as well
-      end,
-    })
-    :find()
-end
+local telescope = require("telescope-hierarchy.telescope")
 
 local M = {}
 
 M.show = function(opts)
   tree.new(function(root)
-    local list = root:to_list()
-    local themed_opts = themes.get_dropdown(opts)
-    show_hierarchy(list, themes.get_dropdown(themed_opts))
+    telescope.show_hierarchy(root:to_list(), opts)
   end)
 end
 
